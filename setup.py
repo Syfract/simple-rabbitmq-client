dcd3bed33f81589e53853a89b150cf3dfcf5a6e8
Ali Mirlou <alimirlou@gmail.com>
2022-09-15 00:42:05 +0430

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rabbitmq_client",
-    version="0.4",
+    version="0.5.0",
     packages=find_packages(),
-    install_requires="pika==1.2.0"
+    install_requires="pika==1.2.1"
 )
